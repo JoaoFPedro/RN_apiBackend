f8cb407263bbe24dd61ec31d4b638e30b41d0f4f
Joao Pedro <joao.fpsilva98@gmail.com>
2024-11-24 11:12:07 -0300

    .

--- src/controller/CreateaCustomerController.ts
+++ src/controller/CreateaCustomerController.ts
@@ -3,12 +3,12 @@ import { CreateCustomerService } from "../services/CreateCustomerService";
 
 class CreateCustomerController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
-    const {name, email} = request.body as {name: string, email: string}
+    const { name, email } = request.body as { name: string; email: string };
     const customerService = new CreateCustomerService();
-    const customer = await customerService.execute({name, email});
+    const customer = await customerService.execute({ name, email });
 
     reply.send(customer);
   }
 }
 
-export default CreateCustomerController
+export default CreateCustomerController;
